9dc7629191f3d0d63fcc62b37389b31a4d9818c9
Synhro2001 <82532564+Synhro2001@users.noreply.github.com>
2022-01-17 10:03:46 +0200

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Lab3.rc
+//
+#define IDD_MAINDIALOG                  101
+#define IDC_PAUSE                       1001
+#define IDC_IDC_CLOCK                   1002
+#define IDC_CLOCK                       1002
+#define IDC_COMMANDLINE                 1003
+#define IDC_BROWSE                      1004
+#define IDC_TIME                        1005
+#define IDC_TIMELIST                    1006
+#define IDC_ADD                         1007
+#define IDC_DELETE                      1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
